46dd7831a06fe40ed7d6d96e40fdc0bce982aa06
Hossain <arafat.kd99@gmail.com>
2024-03-20 20:15:11 +0100

    Add static pages controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
